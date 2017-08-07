7a5b00bee8b4e253baffe2345b44f94d3be180ac
gsmith23 <gsmith23@ph.ed.ac.uk>
2017-08-07 13:37:54 +0100

    minor

--- TLab.C
+++ TLab.C
@@ -973,7 +973,7 @@ void TLab::GraphAsymmetry(Char_t option){
   // half resolution in dPhi 
   // !!to do - access alpha1 from user input
 
-  Float_t alpha1   = DegToRad()*26.0;
+  Float_t alpha1   = DegToRad()*26.0*Sqrt(2);
 
   // half resolution in theta
 
@@ -1147,16 +1147,12 @@ void TLab::GraphAsymmetry(Char_t option){
   grAsym[2]->SetMarkerColor(kGreen+2);
 
   TLegend * leg =  new TLegend(0.6,0.8,0.9,0.9);
- 
-
-  TString theoryLegendTitle = "theory curve #alpha_{#Delta#phi} = 26^{o}";
 
   TString theoryLegendTitle = " ";
   
   alpha1 = alpha1*RadToDeg();
 
-  theoryLegendTitle.Form("theory curve #alpha_{#Delta#phi} = %f^{o}", alpha1);
-
+  theoryLegendTitle.Form("theory curve #alpha_{#Delta#phi} = %.1f^{o}", alpha1);
   
   Char_t yAxis[128];
   
